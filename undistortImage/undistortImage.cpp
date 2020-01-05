466d2851a50c16aa267f0c4d99d1de94703a1f72
todd <chentodd2011@yahoo.com.tw>
2020-01-05 15:13:15 +0800

    Create undistortImage example with OpenCV.

---
+++ undistortImage/undistortImage.cpp
@@ -0,0 +1,52 @@
+// Std headers
+#include <string>
+#include <iostream>
+
+// OpenCV headers
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char **argv)
+{
+    // Image file name
+    std::string image_file = "../distorted.png";
+
+    // Distored coefficients
+    double k1 = -0.28340811, k2 = 0.07395907, p1 = 0.00019359, p2 = 1.76187114e-05;
+
+    // Instrinsics coefficients
+    double fx = 458.654, fy = 457.296, cx = 367.215, cy = 248.375;
+
+    // Read gray scale image, CV_8UC1
+    cv::Mat image = cv::imread(image_file, cv::IMREAD_GRAYSCALE);
+    int rows = image.rows;
+    int cols = image.cols;
+    cv::Mat image_undistort = cv::Mat(rows, cols, CV_8UC1);
+
+    // Calculate the undistort image
+    for (int v = 0; v < rows; ++v)
+    {
+        for (int u = 0; u < cols; ++u)
+        {
+            double x = (u - cx) / fx;
+            double y = (v - cy) / fy;
+            double r = std::sqrt(x * x + y * y);
+            double x_distorted = x * (1 + k1 * r * r + k2 * r * r * r * r) + 2 * p1 * x * y + p2 * (r * r + 2 * x * x);
+            double y_distorted = y * (1 + k1 * r * r + k2 * r * r * r * r) + p1 * (r * r + 2 * y * y) + 2 * p2 * x * y;
+            double u_distorted = fx * x_distorted + cx;
+            double v_distorted = fy * y_distorted + cy;
+
+            if (u_distorted >= 0 && v_distorted >= 0 && u_distorted < cols && v_distorted < rows)
+            {
+                image_undistort.at<uchar>(v, u) = image.at<uchar>(v_distorted, u_distorted);
+            }
+            else
+                image_undistort.at<uchar>(v, u) = 0;
+        }
+    }
+
+    cv::imshow("distored", image);
+    cv::imshow("undistored", image_undistort);
+    cv::waitKey();
+
+    return 0;
+}
